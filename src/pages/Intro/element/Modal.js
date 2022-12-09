6656ff50a11b618d2fe081b287f6371cc90e8822
idenfel <110656247+hanul-dev@users.noreply.github.com>
2022-12-09 18:33:45 +0900

    add pages components

---
+++ src/pages/Intro/element/Modal.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+
+const Modal = ({ children, modal }) => {
+  const styles = { modal };
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Fragment>
+          <StModal {...styles}>{children}</StModal>
+          <StBackDrop {...styles}></StBackDrop>
+        </Fragment>,
+        document.getElementById("root")
+      )}
+    </>
+  );
+};
+
+export default Modal;
+
+const StModal = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  z-index: 20;
+  transform: translate(-50%, -50%);
+  display: ${({ modal }) => {
+    return modal ? "flex" : "none";
+  }};
+  width: 400px;
+  height: 300px;
+  background-color: white;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  border-radius: 12px;
+  box-shadow: 2px 2px 6px black;
+`;
+
+const StBackDrop = styled.div`
+  position: fixed;
+  margin: 0;
+  padding: 0;
+  display: ${({ modal }) => {
+    return modal ? "block" : "none";
+  }};
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(141, 141, 141, 0.8);
+`;
