591522b40f4e5c4e0db41c3f2a8d40f55f416236
hyemin park <hyeminpak@naver.com>
2022-12-14 17:44:36 +0900

    detail 페이지 crud 완료

--- src/redux/config/postStore.js
+++ src/redux/config/postStore.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "../modules/postSlice";
 import commentsReducer from "../modules/commentsSlice";
+import detailReducer from "../modules/detailSlice";
 
 export const postStore = configureStore({
-  reducer: { postReducer, commentsReducer },
+  reducer: { postReducer, commentsReducer, detailReducer },
 });
